73472f68a88e84b1c7ec5aa85ab858d70f95f93f
dominikus1993 <dominikus1910@gmail.com>
2015-04-16 23:01:44 +0200

    Add domain model and controler factory

---
+++ NewSport.WebApi/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewSport.Domain.Api;
+
+namespace NewSport.WebApi.Controllers
+{
+    public class PostController : Controller
+    {
+        private IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View(_postRepository.Posts);
+        }
+    }
+}
--- NewSport.WebApi/Global.asax.cs
+++ NewSport.WebApi/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NewSport.WebApi.Infrastructure;
 
 namespace NewSport.WebApi
 {
@@ -16,6 +17,7 @@ namespace NewSport.WebApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ControllerBuilder.Current.SetControllerFactory(new NewSportControllerFactory());
         }
     }
 }
